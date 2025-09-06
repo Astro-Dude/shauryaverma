f945e1c5db67f5d74fa6766f4a3faa9bdd00e0d4
AstroDude <sagittariusshaurya5@gmail.com>
2025-09-06 19:05:42 +0530

    skeleton fixes

--- src/components/SkeletonLoader.jsx
+++ src/components/SkeletonLoader.jsx
@@ -63,7 +63,7 @@ const SkeletonLoader = ({ type = 'default' }) => {
     return (
       <div className="bg-black">
         {/* Audio Control Skeleton */}
-        <div className="fixed bottom-4 left-4 z-50">
+        <div className="fixed bottom-4 right-4 z-50">
           <div className="bg-gray-800 p-3 rounded-full w-12 h-12 animate-pulse"></div>
         </div>
 
